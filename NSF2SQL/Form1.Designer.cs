850c104d356388a3983f40faddd39edb21394748
Andrew Lesh <lesh.andrew@columbusga.org>
2024-01-23 16:43:56 -0500

    Add Field Name, From, and To fields to filter by

--- NSF2SQL/Form1.Designer.cs
+++ NSF2SQL/Form1.Designer.cs
@@ -42,15 +42,31 @@
             this.gpbAttachments = new System.Windows.Forms.GroupBox();
             this.txbAttachmentsFolder = new System.Windows.Forms.TextBox();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.bFilterBox = new System.Windows.Forms.GroupBox();
+            this.pFrom = new System.Windows.Forms.Panel();
+            this.dtFrom = new System.Windows.Forms.DateTimePicker();
+            this.lFrom = new System.Windows.Forms.Label();
+            this.pFieldName = new System.Windows.Forms.Panel();
+            this.tbFieldName = new System.Windows.Forms.TextBox();
+            this.lFieldName = new System.Windows.Forms.Label();
+            this.dtTo = new System.Windows.Forms.DateTimePicker();
+            this.lTo = new System.Windows.Forms.Label();
+            this.pTo = new System.Windows.Forms.Panel();
+            this.ttFieldName = new System.Windows.Forms.ToolTip(this.components);
             this.gpbAttachments.SuspendLayout();
+            this.bFilterBox.SuspendLayout();
+            this.pFrom.SuspendLayout();
+            this.pFieldName.SuspendLayout();
+            this.pTo.SuspendLayout();
             this.SuspendLayout();
             // 
             // bGetDatabases
             // 
             this.bGetDatabases.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.bGetDatabases.Location = new System.Drawing.Point(11, 278);
+            this.bGetDatabases.Location = new System.Drawing.Point(16, 629);
+            this.bGetDatabases.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.bGetDatabases.Name = "bGetDatabases";
-            this.bGetDatabases.Size = new System.Drawing.Size(123, 23);
+            this.bGetDatabases.Size = new System.Drawing.Size(184, 35);
             this.bGetDatabases.TabIndex = 2;
             this.bGetDatabases.Text = "Search Server";
             this.bGetDatabases.UseVisualStyleBackColor = true;
@@ -59,9 +75,10 @@
             // bExportDocuments
             // 
             this.bExportDocuments.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.bExportDocuments.Location = new System.Drawing.Point(269, 278);
+            this.bExportDocuments.Location = new System.Drawing.Point(404, 629);
+            this.bExportDocuments.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.bExportDocuments.Name = "bExportDocuments";
-            this.bExportDocuments.Size = new System.Drawing.Size(123, 23);
+            this.bExportDocuments.Size = new System.Drawing.Size(184, 35);
             this.bExportDocuments.TabIndex = 4;
             this.bExportDocuments.Text = "Export Documents";
             this.bExportDocuments.UseVisualStyleBackColor = true;
@@ -70,9 +87,10 @@
             // bBrowse
             // 
             this.bBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.bBrowse.Location = new System.Drawing.Point(140, 278);
+            this.bBrowse.Location = new System.Drawing.Point(210, 629);
+            this.bBrowse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.bBrowse.Name = "bBrowse";
-            this.bBrowse.Size = new System.Drawing.Size(123, 23);
+            this.bBrowse.Size = new System.Drawing.Size(184, 35);
             this.bBrowse.TabIndex = 3;
             this.bBrowse.Text = "Search Computer";
             this.bBrowse.UseVisualStyleBackColor = true;
@@ -93,13 +111,14 @@
             this.treeView1.HideSelection = false;
             this.treeView1.ImageIndex = 0;
             this.treeView1.ImageList = this.imageList1;
-            this.treeView1.Location = new System.Drawing.Point(12, 25);
+            this.treeView1.Location = new System.Drawing.Point(18, 39);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.treeView1.Name = "treeView1";
             this.treeView1.SelectedImageIndex = 0;
             this.treeView1.ShowNodeToolTips = true;
             this.treeView1.ShowPlusMinus = false;
             this.treeView1.ShowRootLines = false;
-            this.treeView1.Size = new System.Drawing.Size(380, 188);
+            this.treeView1.Size = new System.Drawing.Size(568, 323);
             this.treeView1.TabIndex = 1;
             this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
             // 
@@ -113,9 +132,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(18, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.Size = new System.Drawing.Size(123, 20);
             this.label1.TabIndex = 5;
             this.label1.Text = "NSF Databases";
             // 
@@ -128,9 +148,10 @@
             // btnBrowseAttachmentsFolder
             // 
             this.btnBrowseAttachmentsFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnBrowseAttachmentsFolder.Location = new System.Drawing.Point(6, 24);
+            this.btnBrowseAttachmentsFolder.Location = new System.Drawing.Point(9, 37);
+            this.btnBrowseAttachmentsFolder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnBrowseAttachmentsFolder.Name = "btnBrowseAttachmentsFolder";
-            this.btnBrowseAttachmentsFolder.Size = new System.Drawing.Size(40, 23);
+            this.btnBrowseAttachmentsFolder.Size = new System.Drawing.Size(60, 35);
             this.btnBrowseAttachmentsFolder.TabIndex = 7;
             this.btnBrowseAttachmentsFolder.Text = "...";
             this.btnBrowseAttachmentsFolder.UseVisualStyleBackColor = true;
@@ -142,9 +163,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.gpbAttachments.Controls.Add(this.txbAttachmentsFolder);
             this.gpbAttachments.Controls.Add(this.btnBrowseAttachmentsFolder);
-            this.gpbAttachments.Location = new System.Drawing.Point(15, 219);
+            this.gpbAttachments.Location = new System.Drawing.Point(18, 536);
+            this.gpbAttachments.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.gpbAttachments.Name = "gpbAttachments";
-            this.gpbAttachments.Size = new System.Drawing.Size(377, 53);
+            this.gpbAttachments.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gpbAttachments.Size = new System.Drawing.Size(568, 82);
             this.gpbAttachments.TabIndex = 8;
             this.gpbAttachments.TabStop = false;
             this.gpbAttachments.Text = "Select directory for attachments";
@@ -153,29 +176,163 @@
             // 
             this.txbAttachmentsFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txbAttachmentsFolder.Location = new System.Drawing.Point(53, 26);
+            this.txbAttachmentsFolder.Location = new System.Drawing.Point(80, 40);
+            this.txbAttachmentsFolder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txbAttachmentsFolder.Name = "txbAttachmentsFolder";
             this.txbAttachmentsFolder.ReadOnly = true;
-            this.txbAttachmentsFolder.Size = new System.Drawing.Size(318, 20);
+            this.txbAttachmentsFolder.Size = new System.Drawing.Size(477, 26);
             this.txbAttachmentsFolder.TabIndex = 8;
+            // 
+            // bFilterBox
+            // 
+            this.bFilterBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bFilterBox.Controls.Add(this.pTo);
+            this.bFilterBox.Controls.Add(this.pFrom);
+            this.bFilterBox.Controls.Add(this.pFieldName);
+            this.bFilterBox.Location = new System.Drawing.Point(18, 369);
+            this.bFilterBox.Name = "bFilterBox";
+            this.bFilterBox.Size = new System.Drawing.Size(568, 157);
+            this.bFilterBox.TabIndex = 9;
+            this.bFilterBox.TabStop = false;
+            this.bFilterBox.Text = "Filter Documents";
+            // 
+            // pFrom
+            // 
+            this.pFrom.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pFrom.Controls.Add(this.dtFrom);
+            this.pFrom.Controls.Add(this.lFrom);
+            this.pFrom.Location = new System.Drawing.Point(6, 84);
+            this.pFrom.Name = "pFrom";
+            this.pFrom.Size = new System.Drawing.Size(284, 69);
+            this.pFrom.TabIndex = 1;
+            // 
+            // dtFrom
+            // 
+            this.dtFrom.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtFrom.Checked = false;
+            this.dtFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtFrom.Location = new System.Drawing.Point(2, 31);
+            this.dtFrom.Name = "dtFrom";
+            this.dtFrom.ShowCheckBox = true;
+            this.dtFrom.Size = new System.Drawing.Size(270, 26);
+            this.dtFrom.TabIndex = 1;
+            this.ttFieldName.SetToolTip(this.dtFrom, "Optional, inclusive date to retrieve documents after");
+            // 
+            // lFrom
+            // 
+            this.lFrom.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lFrom.AutoSize = true;
+            this.lFrom.Location = new System.Drawing.Point(4, 6);
+            this.lFrom.Name = "lFrom";
+            this.lFrom.Size = new System.Drawing.Size(46, 20);
+            this.lFrom.TabIndex = 0;
+            this.lFrom.Text = "From";
+            // 
+            // pFieldName
+            // 
+            this.pFieldName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pFieldName.Controls.Add(this.tbFieldName);
+            this.pFieldName.Controls.Add(this.lFieldName);
+            this.pFieldName.Location = new System.Drawing.Point(6, 24);
+            this.pFieldName.Name = "pFieldName";
+            this.pFieldName.Size = new System.Drawing.Size(556, 54);
+            this.pFieldName.TabIndex = 0;
+            this.ttFieldName.SetToolTip(this.pFieldName, "Date field in Lotus Notes to query against");
+            // 
+            // tbFieldName
+            // 
+            this.tbFieldName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbFieldName.Location = new System.Drawing.Point(2, 23);
+            this.tbFieldName.Name = "tbFieldName";
+            this.tbFieldName.Size = new System.Drawing.Size(549, 26);
+            this.tbFieldName.TabIndex = 1;
+            this.ttFieldName.SetToolTip(this.tbFieldName, "Date field in LN to query on. ");
+            // 
+            // lFieldName
+            // 
+            this.lFieldName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lFieldName.AutoSize = true;
+            this.lFieldName.Location = new System.Drawing.Point(4, 0);
+            this.lFieldName.Name = "lFieldName";
+            this.lFieldName.Size = new System.Drawing.Size(89, 20);
+            this.lFieldName.TabIndex = 0;
+            this.lFieldName.Text = "Field Name";
+            // 
+            // dtTo
+            // 
+            this.dtTo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtTo.Checked = false;
+            this.dtTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtTo.Location = new System.Drawing.Point(2, 31);
+            this.dtTo.Name = "dtTo";
+            this.dtTo.ShowCheckBox = true;
+            this.dtTo.Size = new System.Drawing.Size(252, 26);
+            this.dtTo.TabIndex = 1;
+            this.ttFieldName.SetToolTip(this.dtTo, "Optional, inclusive date to retrieve documents before");
+            // 
+            // lTo
+            // 
+            this.lTo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lTo.AutoSize = true;
+            this.lTo.Location = new System.Drawing.Point(4, 8);
+            this.lTo.Name = "lTo";
+            this.lTo.Size = new System.Drawing.Size(27, 20);
+            this.lTo.TabIndex = 0;
+            this.lTo.Text = "To";
+            // 
+            // pTo
+            // 
+            this.pTo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pTo.Controls.Add(this.dtTo);
+            this.pTo.Controls.Add(this.lTo);
+            this.pTo.Location = new System.Drawing.Point(296, 84);
+            this.pTo.Name = "pTo";
+            this.pTo.Size = new System.Drawing.Size(266, 69);
+            this.pTo.TabIndex = 2;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 310);
+            this.ClientSize = new System.Drawing.Size(606, 678);
             this.Controls.Add(this.gpbAttachments);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.bBrowse);
             this.Controls.Add(this.bExportDocuments);
             this.Controls.Add(this.bGetDatabases);
+            this.Controls.Add(this.bFilterBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(420, 167);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimumSize = new System.Drawing.Size(619, 227);
             this.Name = "Form1";
             this.Text = "NSF 2 SQL";
             this.gpbAttachments.ResumeLayout(false);
             this.gpbAttachments.PerformLayout();
+            this.bFilterBox.ResumeLayout(false);
+            this.pFrom.ResumeLayout(false);
+            this.pFrom.PerformLayout();
+            this.pFieldName.ResumeLayout(false);
+            this.pFieldName.PerformLayout();
+            this.pTo.ResumeLayout(false);
+            this.pTo.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,6 +352,17 @@
         private System.Windows.Forms.GroupBox gpbAttachments;
         private System.Windows.Forms.TextBox txbAttachmentsFolder;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.GroupBox bFilterBox;
+        private System.Windows.Forms.Panel pFieldName;
+        private System.Windows.Forms.TextBox tbFieldName;
+        private System.Windows.Forms.Label lFieldName;
+        private System.Windows.Forms.Panel pFrom;
+        private System.Windows.Forms.DateTimePicker dtFrom;
+        private System.Windows.Forms.Label lFrom;
+        private System.Windows.Forms.Panel pTo;
+        private System.Windows.Forms.DateTimePicker dtTo;
+        private System.Windows.Forms.Label lTo;
+        private System.Windows.Forms.ToolTip ttFieldName;
     }
 }
 
